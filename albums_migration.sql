d8cba2ccce4cd7f1935aa48fda83ce0f8717304e
Pablo Pinero <pinerop@gmail.com>
2015-03-27 16:01:51 -0500

    albums table

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	artist_is VARCHAR(100) NOT NULL,
+	name_of_artist VARCHAR(100) NOT NULL,
+	release_year INT(4) NOT NULL,
+	sales_total FLOAT NOT NULL,
+	genre VARCHAR(30) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
