213b2ba2351a012550fff2d1fae8b55f32deae17
Pablo Pinero <pinerop@gmail.com>
2015-03-31 11:53:31 -0500

    group exercise 3

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,13 +1,34 @@
-SELECT * FROM employees;
+SELECT first_name, last_name
+FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+AND (gender = 'M')
+ORDER BY first_name;
 
-SELECT DISTINCT title
-FROM titles
-GROUP BY title;
+SELECT first_name, last_name
+FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+AND (gender = 'M')
+ORDER BY first_name, last_name;
 
-SELECT last_name	 
+SELECT first_name, last_name
+FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+AND (gender = 'M')
+ORDER BY last_name, first_name;
+
+SELECT * 
 FROM employees WHERE last_name LIKE '%e' 
-AND last_name LIKE 'e%'
-GROUP BY first_name, last_name;
+OR last_name LIKE 'e%'
+ORDER BY emp_no;
+
+SELECT * 
+FROM employees WHERE hire_date LIKE '199%' 
+AND birth_date LIKE '%12-25'
+ORDER BY hire_date DESC;
+
+SELECT * FROM employees WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+
+
 
 
 
