2a24f2270ca692a255a81039b6dec2c0730a651d
Pablo Pinero <pinerop@gmail.com>
2015-03-31 11:46:53 -0500

    group by exercise

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -2,4 +2,12 @@ SELECT * FROM employees;
 
 SELECT DISTINCT title
 FROM titles
-ORDER BY title;
+GROUP BY title;
+
+SELECT last_name	 
+FROM employees WHERE last_name LIKE '%e' 
+AND last_name LIKE 'e%'
+GROUP BY first_name, last_name;
+
+
+
