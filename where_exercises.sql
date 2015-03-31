56f12d4c6e1f30262ed8299679c96290526a8a92
Pablo Pinero <pinerop@gmail.com>
2015-03-31 09:16:35 -0500

    where exercise 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,1 +1,1 @@
-SELECT first_name FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya'
+SELECT first_name FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M'
