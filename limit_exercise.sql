1959f162176c54418070a064df34c8f2d709721a
Pablo Pinero <pinerop@gmail.com>
2015-03-31 13:45:14 -0500

    limit exercise

---
+++ limit_exercise.sql
@@ -0,0 +1,10 @@
+SELECT last_name 
+FROM employees
+GROUP BY last_name DESC 
+LIMIT 10;
+
+SELECT * 
+FROM employees WHERE hire_date LIKE '199%' 
+AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC	, hire_date DESC	
+LIMIT 5;
